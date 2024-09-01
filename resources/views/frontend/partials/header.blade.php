25a720476a120887631c66a7f4eb4aad63730cb1
ahamedsabbir <isratahanedsabbir@gmail.com>
2024-09-01 09:40:11 +0600

    first commit

---
+++ resources/views/frontend/partials/header.blade.php
@@ -0,0 +1,42 @@
+<header>
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-3">
+                <!-- logo  -->
+                <a href="index.html" class="logo">
+                    <img src="{{asset('frontend')}}/assets/images/logo.svg" alt="">
+                </a>
+            </div>
+            <div class="col-9">
+                <div class="menu--wrapper">
+                    <!-- menu  -->
+                    <ul class="menu">
+                        <li>
+                            <a href="index.html" class="active">Home</a>
+                        </li>
+                        <li>
+                            <a href="listing.html">Car Listing</a>
+                        </li>
+                        <li>
+                            <a href="about.html">About Us</a>
+                        </li>
+                        <li>
+                            <a href="faq.html">FAQs</a>
+                        </li>
+                    </ul>
+                    <!-- button area  -->
+                    <div class="btn--area d-flex align-items-center gap-3">
+                        <a href="login.html" class="button">Log In</a>
+                        <a href="register.html" class="buttonv2">Register</a>
+                    </div>
+                </div>
+                <!-- hamburger menu  -->
+                <div class="hamburger-menu">
+                    <span class="line-top"></span>
+                    <span class="line-center"></span>
+                    <span class="line-bottom"></span>
+                    </div>
+            </div>
+        </div>
+    </div>
+</header>
