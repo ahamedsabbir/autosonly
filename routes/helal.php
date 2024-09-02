2c9cadf2b4c6a0d5762d52d86e7fafcfa743647d
helal <husoaib422@gmail.com>
2024-09-02 08:25:03 +0600

    some change

--- routes/helal.php
+++ routes/helal.php
@@ -2,12 +2,14 @@
 use App\Http\Controllers\Web\Backend\CarController;
 use App\Http\Controllers\Web\Backend\DashboardController;
 
-Route::get('/admin',[DashboardController::class,'index'])->name('admin'); 
-Route::get('/car',[CarController::class,'index'])->name('car'); 
+// Route::get('/admin',[DashboardController::class,'index'])->name('admin'); 
+// Route::get('/car',[CarController::class,'index'])->name('car'); 
 
 
 // Route::resource('car',CarController::class)->names('car');
 
-// Route::group(['prefix' => 'dashboard'], function () {
+Route::group(['prefix' => 'admin'], function () {
 //    Route::get('/',[DashboardController::class,'index'])->name('dashboard'); 
-// });
+Route::get('/',[DashboardController::class,'index'])->name('admin'); 
+Route::get('/car',[CarController::class,'index'])->name('car'); 
+});
