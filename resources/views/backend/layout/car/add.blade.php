4a61dbc55e625929c57d29d6df3d6604b812f64e
helal <husoaib422@gmail.com>
2024-09-03 17:23:48 +0600

    add all file last

--- resources/views/backend/layout/car/add.blade.php
+++ resources/views/backend/layout/car/add.blade.php
@@ -93,6 +93,7 @@
               <i class="fas fa-plus"></i>
               <span>Add files</span>
             </span>
+
           </div>
            <!-- ===========================image upload previews ================================= -->
           <div class="table table-striped files" id="previews">
@@ -226,37 +227,25 @@
     clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
     })
 
-    myDropzone.on("addedfile", function (file) {
-    // Hookup the start button
-    file.previewElement.querySelector(".start").onclick = function () { myDropzone.enqueueFile(file) }
-    })
 
-    // Update the total progress bar
-    myDropzone.on("totaluploadprogress", function (progress) {
-    document.querySelector("#total-progress .progress-bar").style.width = progress + "%"
-    })
+var token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
-    myDropzone.on("sending", function (file) {
-    // Show the total progress bar when upload starts
-    document.querySelector("#total-progress").style.opacity = "1"
-    // And disable the start button
-    file.previewElement.querySelector(".start").setAttribute("disabled", "disabled")
-    })
+var myDropzone = new Dropzone(document.body, {
+    url: "/upload",
+    headers: {
+        'X-CSRF-TOKEN': token
+    },
+    thumbnailWidth: 80,
+    thumbnailHeight: 80,
+    parallelUploads: 20,
+    previewTemplate: previewTemplate,
+    autoQueue: false,
+    previewsContainer: "#previews",
+    clickable: ".fileinput-button"
+});
 
-    // Hide the total progress bar when nothing's uploading anymore
-    myDropzone.on("queuecomplete", function (progress) {
-    document.querySelector("#total-progress").style.opacity = "0"
-    })
 
-    // Setup the buttons for all transfers
-    // The "add files" button doesn't need to be setup because the config
-    // `clickable` has already been specified.
-    document.querySelector("#actions .start").onclick = function () {
-    myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED))
-    }
-    document.querySelector("#actions .cancel").onclick = function () {
-    myDropzone.removeAllFiles(true)
-    }
-    // DropzoneJS Demo Code End
+    
+    // DropzoneJS Code End
   </script>
 @endpush
